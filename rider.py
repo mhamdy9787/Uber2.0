cbf309ae55f2e87fc21f9f3240f9c303a5cf92ca
mhamdy9787 <mhamdy9787@gmail.com>
2016-02-17 18:42:58 -0800

    Create rider.py

---
+++ rider.py
@@ -0,0 +1,53 @@
+"""
+The rider module contains the Rider class. It also contains
+constants that represent the status of the rider.
+
+=== Constants ===
+@type WAITING: str
+    A constant used for the waiting rider status.
+@type CANCELLED: str
+    A constant used for the cancelled rider status.
+@type SATISFIED: str
+    A constant used for the satisfied rider status
+"""
+
+WAITING = "waiting"
+CANCELLED = "cancelled"
+SATISFIED = "satisfied"
+
+
+class Rider:
+
+    def __init__(self,identifier,status,destination,origin,patience):
+        """Initializes the Rider class
+
+        @type self: Rider
+        @type identifier; str
+        @type status: str
+        @type destination: Location
+        @type origin: Location
+        @type patience: int
+        @rtype: None
+        """
+        self.id = identifier
+        self.status = status
+        self.destination = destination #locationClass
+        self.location = origin #locationClass
+        self.patience = patience
+
+    def updateStatus(self,newStatus):
+        """Changes the status of the rider accordingly.
+
+        @type self: Rider
+        @type newStatus: str
+        @rtype: None
+        """
+        self.status = newStatus
+
+    def __str__(self):
+        """Return a string representation.
+
+        @type self: Rider
+        @rtype: str
+        """
+        return "{} {}".format(self.id,self.status)
