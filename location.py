0bdd5beb5a5aedb5fe1af745133ea542ae5b0e69
mhamdy9787 <mhamdy9787@gmail.com>
2016-02-18 18:25:32 -0800

    Update location.py

--- location.py
+++ location.py
@@ -16,6 +16,8 @@ class Location:
         """Return a string representation.
 
         @rtype: str
+        >>> print(Location(5,10))
+        5,10
         """
         # TODO
         return "{},{}".format(self.row,self.column)
@@ -24,13 +26,13 @@ class Location:
         """Return True if self equals other, and false otherwise.
 
         @rtype: bool
-        >>>location1 = Location(5,5)
-        >>>location2 = Location(5,5)
-        >>>location1 == location2
+        >>> location1 = Location(5,5)
+        >>> location2 = Location(5,5)
+        >>> location1 == location2
         True
-        >>>location1 = Location(8,9)
-        >>>location2 = Location(5,5)
-        >>>location1 == location2
+        >>> location1 = Location(8,9)
+        >>> location2 = Location(5,5)
+        >>> location1 == location2
         False
         """
         # TODO
@@ -43,9 +45,9 @@ def manhattan_distance(origin, destination):
     @type origin: Location
     @type destination: Location
     @rtype: int
-    >>>origin1 = Location(6,9)
-    >>>destination1 = Location(12,18)
-    >>>manhattan_distance(origin1,destination1)
+    >>> origin1 = Location(6,9)
+    >>> destination1 = Location(12,18)
+    >>> manhattan_distance(origin1,destination1)
     15
     """
     # TODO
@@ -58,9 +60,9 @@ def deserialize_location(location_str):
     @type location_str: str
         A location in the format 'row,col'
     @rtype: Location
-    >>>location_str1 = "2,5"
-    >>>deserialize_location(location_str1)
-    Location(2,5)
+    >>> location_str1 = "2,5"
+    >>> print(deserialize_location(location_str1))
+    2,5
     """
     # TODO
     location  = Location(int(location_str[0]),int(location_str[2]))
